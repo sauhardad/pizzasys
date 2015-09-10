eb699e52fe827444fa05b4c914d5052359a27811
Md Salman Ahmed <ahmedm@goldmail.etsu.edu>
2015-09-10 17:15:29 -0400

    Updated pizzasys database and added table creation command.

--- pizzasys_db.sql
+++ pizzasys_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 05, 2015 at 05:27 PM
+-- Generation Time: Sep 10, 2015 at 11:12 PM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `pizzasys_db`
 --
-CREATE DATABASE IF NOT EXISTS `pizzasys_db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `pizzasys_db`;
 
 -- --------------------------------------------------------
 
@@ -28,7 +26,6 @@ USE `pizzasys_db`;
 -- Table structure for table `ci_sessions`
 --
 
-DROP TABLE IF EXISTS `ci_sessions`;
 CREATE TABLE IF NOT EXISTS `ci_sessions` (
   `session_id` varchar(40) NOT NULL DEFAULT '0',
   `ip_address` varchar(45) NOT NULL DEFAULT '0',
@@ -44,7 +41,56 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
 --
 
 INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('ff6870428730c49cc8d0d52ea0ff3791', '127.0.0.1', 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36', 1441417081, '');
+('f4e511f73087c94edd5da6c3d733b308', '127.0.0.1', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36', 1441919486, 'a:1:{s:9:"user_data";s:0:"";}');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_order`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_order` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `size` int(1) NOT NULL,
+  `crush` int(1) DEFAULT NULL,
+  `sauce` int(1) NOT NULL,
+  `cheese` int(1) NOT NULL,
+  `topping` varchar(255) NOT NULL,
+  `order_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tbl_order`
+--
+
+INSERT INTO `tbl_order` (`order_id`, `size`, `crush`, `sauce`, `cheese`, `topping`, `order_time`) VALUES
+(1, 2, 1, 3, 2, 'topping', '2015-09-10 21:08:42');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `security_question` varchar(255) DEFAULT NULL,
+  `security_answer` varchar(255) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  `entry_date` datetime DEFAULT NULL,
+  `last_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`user_id`, `email`, `password`, `security_question`, `security_answer`, `active`, `entry_date`, `last_login`) VALUES
+(1, 'a@gmail.com', '123', 'A', 'B', 1, '2015-09-10 17:08:42', '2015-09-10 21:08:42');
 
 -- --------------------------------------------------------
 
@@ -52,7 +98,6 @@ INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activ
 -- Table structure for table `tbl_users`
 --
 
-DROP TABLE IF EXISTS `tbl_users`;
 CREATE TABLE IF NOT EXISTS `tbl_users` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
@@ -67,7 +112,33 @@ CREATE TABLE IF NOT EXISTS `tbl_users` (
 --
 
 INSERT INTO `tbl_users` (`id`, `email`, `password`, `last_login`, `entry_timestamp`) VALUES
-(1, 'sauhardad@gmail.com', '$2a$08$FqOeZeD0779QpjUFGeglOe9cO5o2GeL21fL4z/i0FmJTDqV4GkN3.', '2015-09-05 07:25:47', '2015-09-05 01:40:47');
+(1, 'sauhardad@gmail.com', '$2a$08$FqOeZeD0779QpjUFGeglOe9cO5o2GeL21fL4z/i0FmJTDqV4GkN3.', '2015-09-10 17:11:43', '2015-09-10 21:11:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user_order`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_user_order` (
+  `user_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `number` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`,`order_id`),
+  KEY `order_id` (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_user_order`
+--
+ALTER TABLE `tbl_user_order`
+  ADD CONSTRAINT `tbl_user_order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`),
+  ADD CONSTRAINT `tbl_user_order_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `tbl_order` (`order_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
